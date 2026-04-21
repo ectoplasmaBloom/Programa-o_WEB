5022b7f8092d0e31be3c6bbd0e18c1a69d025d08
ectoplasmaBloom <94301842+ectoplasmaBloom@users.noreply.github.com>
2026-04-21 17:33:40 -0300

    Add files via upload

---
+++ JS - AULA 9/lista_de_tarefas/script.js
@@ -0,0 +1,58 @@
+let contador = 0;
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-Add');
+let main = document.getElementById('areaLista');
+
+function addTarefa() {
+    // Pegar o valor digitado no input
+    let valorInput = input.value;
+    //Se não for vazio, nem nulo, nem indefinido
+    if ((valorInput !== '') && (valorInput !== undefined) && (valorInput !== null)) {
+        ++contador;
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class=mdi mdi-circle-outline"></id>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete">Deletar</i>
+            </button>
+        </div>
+    </div>`;
+    //Adicionar o novo item na Lista
+    main.innerHTML += novoItem
+    //Zerar o input
+    input.value = "";
+    input.focus();
+    }
+}
+//Deleta os itens
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class')
+    //Marca item como feito
+    if (classe=="item"){
+        item.classList.add('clicado');
+        var icone = document.getElementById('icone_'+ id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+    } else{
+        item.classList.remove('clicado');
+        var icone = document.getElementById('icone_' + id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
+}
+//teclar enter para adicionar tarefa
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
